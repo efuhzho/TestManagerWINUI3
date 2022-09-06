64dbe759acdba535d5ab602d95d914f76efe91a8
Macro <fuhong.zhou@qq.com>
2022-09-07 04:57:32 +0800

    .

--- TestManager/ViewModels/MainViewModel.cs
+++ TestManager/ViewModels/MainViewModel.cs
@@ -2,6 +2,7 @@
 using CommunityToolkit. Mvvm. ComponentModel;
 using CommunityToolkit. Mvvm. Input;
 using DKCommunicationNET;
+using DKCommunicationNET. Module;
 using Microsoft. UI. Xaml. Controls;
 
 namespace TestManager. ViewModels;
@@ -10,7 +11,7 @@ public partial class MainViewModel : ObservableObject
 {
     public MainViewModel ()
     {
-        portName = ports[0];
+        portName = portNames[0];
         baudRate = baudRates[2];
     }
 
@@ -32,27 +33,67 @@ public partial class MainViewModel : ObservableObject
 
     #endregion
 
-    //丹迪克设备对象
+    #region 串口开关绑定
+    /// <summary>
+    /// 开关的状态属性
+    /// </summary>
+    [ObservableProperty]
+    private bool isOn_PortSwitch;
+    #endregion
+
+    /// <summary>
+    /// 丹迪克设备对象
+    /// </summary>
     [ObservableProperty]
     private Dandick? sS;
 
-    //用户选择的设备型号
+    /// <summary>
+    /// 当前选择的设备型号
+    /// </summary>
     [ObservableProperty]
     private string? sS_Model;
 
+    /// <summary>
+    /// 当前选择的串口号
+    /// </summary>
     [ObservableProperty]
     private string? portName;
 
+    /// <summary>
+    /// 当前设置的波特率
+    /// </summary>
     [ObservableProperty]
     private int baudRate;
+
+    #region 从实例同步的引用类型数据
+
+    /// <summary>
+    /// 下位机回复的型号
+    /// </summary>
+    [ObservableProperty]
+    private string? model;
+
+    /// <summary>
+    /// 交流电压档位集合
+    /// </summary>
+    [ObservableProperty]
+    private float[]? ranges_ACU;
+
+    /// <summary>
+    /// 交流电压档位集合
+    /// </summary>
+    [ObservableProperty]
+    private float[]? ranges_ACI;
+    #endregion
+
 
     #region ComboBox初始化
 
     [ObservableProperty]
-    private string[] dsModels = Enum. GetNames(typeof(DKCommunicationNET. Models));
+    private string[] dandickModels = Enum. GetNames(typeof(DKCommunicationNET. Models));
 
     [ObservableProperty]
-    private string[]? ports = SerialPort. GetPortNames();
+    private string[]? portNames = SerialPort. GetPortNames();
 
     [ObservableProperty]
     private int[]? baudRates = new int[] { 9600 , 19200 , 115200 };
@@ -69,8 +110,7 @@ public partial class MainViewModel : ObservableObject
     [ObservableProperty]
     private int[] dataBits = new int[] { 5 , 6 , 7 , 8 };
 
-    [ObservableProperty]
-    private string? model;
+
 
     #endregion
     /// <summary>
@@ -79,63 +119,73 @@ public partial class MainViewModel : ObservableObject
     [ObservableProperty]
     private bool disableSerialPortEdit = true;
 
-    [RelayCommand]
-    private void ToggleSwitch_Toggled (bool isOn)
+    public async void ToggleSwitch_Toggled (bool isOn)
     {
         switch ( isOn )
         {
+            //正在打开串口
             case true:
+
+                //禁用参数设置控件
+                DisableSerialPortEdit = false;
+                //判断是否选择型号和串口号
+                if ( SS_Model == null || PortName == null )
                 {
-                    DisableSerialPortEdit = false;
+                    InfoBarSeverity = InfoBarSeverity. Warning;
+                    IsInfobarShow = true;
+                    InfobarTitle = "Warning";
+                    InfobarMessage = "还没有选择任何设备型号";
+                    IsOn_PortSwitch = false;
+                    return;
+                }
+                //实例化对象
+                SS = new Dandick(( DKCommunicationNET. Models )Enum. Parse(typeof(DKCommunicationNET. Models) , SS_Model));
+                //初始化串口参数
+                SS. SerialPortInni(PortName , baudRate);
 
-                    if ( SS_Model != null && PortName != null )
+                //打开串口并发送握手报文
+                var result = SS. Open();
+
+                if ( result. IsSuccess )
+                {
+                    InfoBarSeverity = InfoBarSeverity. Success;
+                    IsInfobarShow = false;   //TODO 改为false
+                    InfobarTitle = "Success";
+                    InfobarMessage = result. Content;
+                    IsOn_PortSwitch = true;
+
+                    await Task. Run(() =>
                     {
-                        //实例化对象
-                        SS = new Dandick(( DKCommunicationNET. Models )Enum. Parse(typeof(DKCommunicationNET. Models) , SS_Model));
+                        SS. HandShake();
 
-                        //初始化串口参数
-                        SS. SerialPortInni(PortName , baudRate);
-
-                        //打开串口并发送握手报文
-
-                        try
-                        {
-                            SS. Open();
-                            if ( !SS. IsOpen() )
-                            {
-                                InfoBarSeverity = InfoBarSeverity. Error;
-                                IsInfobarShow = true;
-                                InfobarTitle = "Failed";
-                                InfobarMessage = "串口打开失败。";
-                                return;
-                            }
-                            InfoBarSeverity = InfoBarSeverity. Success;
-                            IsInfobarShow = true;
-                            InfobarTitle = "Success";
-                            InfobarMessage = "串口打开成功。";
-                            Model = SS. Model;
-                        }
-                        catch ( Exception ex )
-                        {
-                            InfoBarSeverity = InfoBarSeverity. Warning;
-                            IsInfobarShow = true;
-                            InfobarTitle = "Warning";
-                            InfobarMessage = ex. Message;
-                        }
-                    }
-                    break;
+                        //获取交流源档位信息
+                        var result_GetRanges = SS. ACS. GetRanges();
+                        SS. DCS. GetRanges();
+                    });
+                    Ranges_ACU = SS. ACS. Ranges_ACU;
+                    Ranges_ACI = SS. ACS.Ranges_ACI;
+                    return;
+                }
+                else
+                {
+                    InfoBarSeverity = InfoBarSeverity. Error;
+                    IsInfobarShow = true;
+                    InfobarTitle = "Failed";
+                    InfobarMessage = result. Message;
+                    IsOn_PortSwitch = false;
+                    return;
                 }
 
+            //正在关闭串口
             case false:
-                {
-                    DisableSerialPortEdit = true;
 
-                    if ( SS != null )
-                    {
-                        SS. Close();
-                    }
-                    break;
+                DisableSerialPortEdit = true;
+                IsOn_PortSwitch = false;
+                if ( SS != null )
+                {
+                    SS. Close();
                 }
+                break;
         }
     }
 
--- TestManager/Views/MainPage.xaml.cs
+++ TestManager/Views/MainPage.xaml.cs
@@ -16,7 +16,20 @@ public sealed partial class MainPage : Page
     {
         ViewModel = App. GetService<MainViewModel>();
         InitializeComponent();
-        Array. Sort(ViewModel. DsModels);
-        ViewModel. SS_Model = "Hex81";
+        Array. Sort(ViewModel. DandickModels);
+    }
+
+
+    private void ToggleSwitch_SS_Toggled (object sender , Microsoft. UI. Xaml. RoutedEventArgs e)
+    {
+        ViewModel. ToggleSwitch_Toggled(ToggleSwitch_SS. IsOn);
+        if ( ViewModel. IsOn_PortSwitch )
+        {
+            ToggleSwitch_SS. IsOn = true;
+        }
+        else
+        {
+            ToggleSwitch_SS. IsOn = false;
+        }
     }
 }
